fe0c89b542968d265c3c8edf1401afc6f9cc4d78
Sekhar <usekhartechie@gmail.com>
2020-03-01 17:11:26 +0530

    added method descriptions to actions

--- generics/actions.ts
+++ generics/actions.ts
@@ -3,6 +3,11 @@ import { WebElementPromise } from 'selenium-webdriver';
 export class WebComponent {
   constructor(protected element: WebElementPromise, public selector: string) { }
 
+
+  /**
+   * this method will be helpful to click on weblement
+   * if element is nto availble to click it will thow the exception 
+   */
   public async click() {
     try {
       return await this.element.click();
@@ -15,10 +20,19 @@ export class WebComponent {
     }
   }
 
+  /**
+   * this library method will helpful to enter
+   * the text into text boxes
+   * @param text 
+   */
   public async enterText(text: string) {
     return await this.element.sendKeys(text);
   }
 
+  /**
+   * this library method will help the webdriver 
+   * to check the element is displayed or not
+   */
   public async isDisplayed(): Promise<boolean> {
     try {
       return await this.element.isDisplayed();
@@ -27,10 +41,18 @@ export class WebComponent {
     }
   }
 
+  /**
+   * this library mwethod will helpfull 
+   * to get the text 
+   */
   public async getText() {
     return await this.element.getText();
   }
 
+  /**
+   * this library method will be helpfull to 
+   * move the cursor on the specified element 
+   */
   public async mouseHover() {
     this.isDisplayed();
     return await (await this.element).getDriver().actions().mouseMove(this.element).perform();
