f6431073150fedb78abcd1f6c5986dda5afba079
Sekhar <usekhartechie@gmail.com>
2020-03-08 00:45:58 +0530

    modified the tests where env variables not readble

--- test/CheckoutProducts.ts
+++ test/CheckoutProducts.ts
@@ -1,11 +1,14 @@
 import { specification, action, when, then } from "../generics/mocha-bdd";
 import { AllPages } from "../pages";
 import { Browser, ensure } from "../generics";
+import * as dotenv from "dotenv";
+
+dotenv.config();
 
 specification('User navigates to application', () => {
     let pages: AllPages;
     action(async () => {
-        pages = new AllPages(new Browser(process.env.browser));
+        pages = new AllPages(new Browser(process.env.BROWSER));
         await pages.home.navigate();
     });
     when('user search for products', async () => {
